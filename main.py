fcb94bd1cb6332e67b9592ae81ec4547391e63d3
LeonPatmore <leon.patmore@vonage.com>
2020-09-07 22:01:11 +0100

    init commit

---
+++ main.py
@@ -0,0 +1,13 @@
+import gspread
+
+
+def main():
+    gc = gspread.service_account(filename="credentials.json")
+    spreadsheet = gc.open_by_url("https://docs.google.com/spreadsheets/d/12vPGwr5Ds3ZygiHf0-XXVXuOZFhH7VTLrWioUroeUbA/edit#gid=0")
+    spreadsheet.worksheet()
+
+    pass
+
+
+if __name__ == '__main__':
+    main()
