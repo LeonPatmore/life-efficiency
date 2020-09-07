4936ad38deefcd130164244e371162188d5d732c
LeonPatmore <leon.patmore@vonage.com>
2020-09-07 22:36:44 +0100

    Add add item function

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 import gspread
 
+from shopping_list import ShoppingListWorksheet
+
 
 def main():
     gc = gspread.service_account(filename="credentials.json")
-    spreadsheet = gc.open_by_url("https://docs.google.com/spreadsheets/d/12vPGwr5Ds3ZygiHf0-XXVXuOZFhH7VTLrWioUroeUbA/edit#gid=0")
-    spreadsheet.worksheet()
+    spreadsheet = gc.open_by_url(
+        "https://docs.google.com/spreadsheets/d/12vPGwr5Ds3ZygiHf0-XXVXuOZFhH7VTLrWioUroeUbA/edit#gid=0")
 
-    pass
+    shopping_list_worksheet = spreadsheet.worksheet("Sheet1")
+    shopping_list = ShoppingListWorksheet(shopping_list_worksheet)
+    shopping_list.add_item("hello")
 
 
 if __name__ == '__main__':
---
+++ shopping_list.py
@@ -0,0 +1,16 @@
+import gspread
+
+
+class ShoppingList(object):
+
+    def add_item(self, item: str):
+        raise NotImplementedError()
+
+
+class ShoppingListWorksheet(ShoppingList):
+
+    def __init__(self, worksheet: gspread.Worksheet):
+        self.worksheet = worksheet
+
+    def add_item(self, item: str):
+        self.worksheet.insert_row([item], 1)
