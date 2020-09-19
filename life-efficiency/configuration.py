b90dbd13be90d39b2cb73fab5050f10d3f31f24a
LeonPatmore <leon.patmore@vonage.com>
2020-09-19 21:14:23 +0100

    Remove useless line spacing

--- life-efficiency/configuration.py
+++ life-efficiency/configuration.py
@@ -3,5 +3,3 @@ import gspread
 gc = gspread.service_account(filename="credentials.json")
 spreadsheet = gc.open_by_url(
     "https://docs.google.com/spreadsheets/d/12vPGwr5Ds3ZygiHf0-XXVXuOZFhH7VTLrWioUroeUbA/edit#gid=0")
-
-
