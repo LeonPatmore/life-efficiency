7844bb512b71ce5809114bbd809b6bc323b38c4c
LeonPatmore <leon.patmore@vonage.com>
2020-09-30 20:46:53 +0100

    Adding tests

--- tests/helpers/test_lambda_splitter.py
+++ tests/helpers/test_lambda_splitter.py
@@ -1,4 +1,34 @@
+from unittest.mock import Mock
+
+import pytest
+
 from helpers.lambda_splitter import LambdaSplitter
+
+PATH_PARAMETER_KEY = 'path_parameter'
+
+
+def _generate_event(path_parameter: str, sub_path: str) -> dict:
+    return {
+        'httpMethod': 'GET',
+        'pathParameters': {
+            path_parameter: sub_path
+        }
+    }
+
+
+@pytest.fixture
+def setup_lambda_splitter_with_method_handler():
+    return_value = {'statusCode': 200}
+    mock_method = Mock(return_value=return_value)
+
+    def handler_method(*args, **kwargs): return mock_method(*args, **kwargs)
+    lambda_splitter = LambdaSplitter(PATH_PARAMETER_KEY)
+    sub_path = 'sub_path'
+    lambda_splitter.add_sub_handler(sub_path, handler_method)
+    return lambda_splitter, mock_method, return_value, sub_path
+
+
+_setup_lambda_splitter_with_method_handler = setup_lambda_splitter_with_method_handler
 
 
 def test_sanitise_path_leading_slash():
@@ -23,3 +53,34 @@ def test_sanitise_method():
     sanitised_method = LambdaSplitter.sanitise_method(some_method)
 
     assert sanitised_method == 'GET'
+
+
+def test_call_when_handler_is_a_method(_setup_lambda_splitter_with_method_handler):
+    lambda_splitter, mock_method, return_value, sub_path = _setup_lambda_splitter_with_method_handler
+
+    result = lambda_splitter.__call__(_generate_event(PATH_PARAMETER_KEY, sub_path), {})
+
+    mock_method.assert_called_once_with()
+    assert result == return_value
+
+
+def test_call_when_no_subpath_match(_setup_lambda_splitter_with_method_handler):
+    lambda_splitter, mock_method, return_value, sub_path = _setup_lambda_splitter_with_method_handler
+
+    result = lambda_splitter.__call__(_generate_event(PATH_PARAMETER_KEY, 'unknown_path'), {})
+
+    mock_method.assert_not_called()
+
+    assert 'statusCode' in result
+    assert result['statusCode'] == 404
+    assert 'body' in result
+    assert result['body'] == "{{\"error\": \"could not find path for this command, possible paths are [ {} ]\"}}"\
+        .format(sub_path)
+
+
+# def test_call_when_handler_is_a_lambda_splitter(_setup_lambda_splitter_with_method_handler):
+#     lambda_splitter, mock_method, return_value, sub_path = _setup_lambda_splitter_with_method_handler
+#
+#     lambda_splitter.__call__(_generate_event(PATH_PARAMETER_KEY, sub_path), {})
+#
+#     mock_method.assert_called_once_with()
