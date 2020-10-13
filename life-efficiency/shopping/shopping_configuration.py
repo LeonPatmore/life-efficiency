80c23d1ce470dd681f598eb9792e1cb1e0e8c77d
LeonPatmore <leon.patmore@vonage.com>
2020-10-13 19:01:11 +0100

    Add meal for wednesday

--- life-efficiency/shopping/shopping_configuration.py
+++ life-efficiency/shopping/shopping_configuration.py
@@ -1,7 +1,7 @@
 from configuration import spreadsheet
 from helpers.datetime import Day
 from shopping.mealplan.meal_plan import MealPlan
-from shopping.shopping_items import CHOCOLATE_MILKSHAKE, APPLES, GRAPES, PEARS, MOUTHWASH, PASTA, PESTO
+from shopping.shopping_items import CHOCOLATE_MILKSHAKE, APPLES, GRAPES, PEARS, MOUTHWASH, PASTA, PESTO, FISH, SALAD
 from shopping.shopping_manager import ShoppingManagerSpreadsheet
 
 repeating_items = [
@@ -14,4 +14,5 @@ repeating_items = [
 
 mean_plan = MealPlan()
 mean_plan.add_meal(Day.MONDAY, [PASTA, PESTO])
+mean_plan.add_meal(Day.WEDNESDAY, [FISH, SALAD])
 shopping_manager = ShoppingManagerSpreadsheet(spreadsheet, mean_plan, repeating_items)
