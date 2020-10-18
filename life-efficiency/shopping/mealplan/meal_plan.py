f9014bdb65505a38750c164afe77e838e0dd6166
LeonPatmore <leon.patmore@vonage.com>
2020-10-18 23:36:48 +0100

    Finish mealplan implementation

--- life-efficiency/shopping/mealplan/meal_plan.py
+++ life-efficiency/shopping/mealplan/meal_plan.py
@@ -1,22 +1,51 @@
-from helpers.datetime import Day
+import logging
+from datetime import datetime
+
+from helpers.datetime import Day, get_current_datetime_utc
 
 
 class MealPlan(object):
 
     def __init__(self):
         self.mean_plan = dict()
-
-    def _load_meal_plans(self):
-        raise NotImplementedError()
+        self._load_meal_plans()
 
     def get_meal_for_day(self, day: Day) -> list:
         return self.mean_plan.get(day, [])
 
-    def is_meal_purchased(self, day: Day) -> bool:
+    def _load_meal_plans(self):
         raise NotImplementedError()
 
-    def purchase_meal(self, day: Day):
+    def _get_purchase_time(self) -> datetime:
         raise NotImplementedError()
+
+    def _reset_purchase_time(self, new_time: datetime):
+        raise NotImplementedError()
+
+    def _is_meal_purchased_implementation(self, day: Day) -> bool:
+        raise NotImplementedError()
+
+    def _purchase_meal_implementation(self, day: Day):
+        raise NotImplementedError()
+
+    def is_meal_purchased(self, day: Day):
+        self._check_purchase_time()
+        return self._is_meal_purchased_implementation(day)
+
+    def purchase_meal(self, day: Day):
+        self._check_purchase_time()
+        return self._purchase_meal_implementation(day)
+
+    def _check_purchase_time(self):
+        current_purchase_time = self._get_purchase_time()
+        current_week = current_purchase_time.isocalendar()[1]
+        today_week = get_current_datetime_utc().isocalendar()[1]
+
+        logging.info("Current purchase week [ {} ], today week [ {} ]", current_week, today_week)
+
+        if current_week != today_week:
+            logging.info("Resetting purchase time!")
+            self._reset_purchase_time(get_current_datetime_utc())
 
     def add_meal(self, day: Day, items: list, append: bool = False):
         if append:
--- life-efficiency/shopping/mealplan/meal_plan_worksheet.py
+++ life-efficiency/shopping/mealplan/meal_plan_worksheet.py
@@ -1,24 +1,41 @@
+from datetime import datetime
+from distutils.util import strtobool
+
 import gspread
 
-from helpers.datetime import Day
+from helpers.datetime import Day, string_to_datetime, datetime_to_string, get_current_datetime_utc
 from shopping.mealplan.meal_plan import MealPlan
 
 
 class MealPlanWorksheet(MealPlan):
 
     def __init__(self, meal_plan_worksheet: gspread.Worksheet, meal_purchase_worksheet: gspread.Worksheet):
+        super().__init__()
         self.meal_plan_worksheet = meal_plan_worksheet
         self.meal_purchase_worksheet = meal_purchase_worksheet
-        self.meal_plans = dict()
+        self._init_worksheet()
+
+    def _init_worksheet(self):
+        if self.meal_purchase_worksheet.get_all_values()[0][0] == "":
+            self._reset_purchase_time(get_current_datetime_utc())
 
     def _load_meal_plans(self):
         worksheet_values = self.meal_plan_worksheet.get_all_values()
         for index, day in enumerate(Day):
             worksheet_row = worksheet_values[index]
-            self.meal_plans[day] = worksheet_row
+            self.mean_plan[day] = worksheet_row
 
-    def is_meal_purchased(self, day: Day) -> bool:
-        pass
+    def _get_purchase_time(self) -> datetime:
+        return string_to_datetime(self.meal_purchase_worksheet.get_all_values()[0][0])
 
-    def purchase_meal(self, day: Day):
-        pass
+    def _reset_purchase_time(self, new_time: datetime):
+        self.meal_purchase_worksheet.update_cell(0, 0, datetime_to_string(new_time))
+        for day in Day:
+            self.meal_purchase_worksheet.update_cell(day.value + 1, 0, "False")
+
+    def _is_meal_purchased_implementation(self, day: Day) -> bool:
+        purchased_string = self.meal_purchase_worksheet.get_all_values()[day.value][0]  # type: str
+        return strtobool(purchased_string)
+
+    def _purchase_meal_implementation(self, day: Day):
+        self.meal_purchase_worksheet.update_cell(day.value, 0, "True")
