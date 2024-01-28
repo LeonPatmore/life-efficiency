0acea19b4032f21aa46b8882a2d24673f8480b1b
Leon Patmore <leon.patmore@bath.edu>
2024-01-28 01:03:39 +0000

    Update datetime.py

--- life-efficiency/helpers/datetime.py
+++ life-efficiency/helpers/datetime.py
@@ -8,6 +8,7 @@ def get_current_datetime_utc() -> datetime:
 
 
 def datetime_to_string(a_datetime: datetime, string_format: str = DEFAULT_DATETIME_FORMAT) -> str:
+    # TODO: This replacement is due to a bug on specific platforms that will not pad year 1 as 0001.
     return a_datetime.strftime(string_format).replace("/1", "/0001")
 
 
