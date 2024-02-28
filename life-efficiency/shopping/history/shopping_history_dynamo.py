54656a704118910b9b5c6ec009d5e33bc90a953e
Leon Patmore <leon.patmore@bath.edu>
2024-02-28 22:48:21 +0000

    Fixing history.

--- life-efficiency/shopping/history/shopping_history_dynamo.py
+++ life-efficiency/shopping/history/shopping_history_dynamo.py
@@ -1,3 +1,5 @@
+import uuid
+
 from helpers.datetime import string_to_datetime, datetime_to_string
 from shopping.history.shopping_history import ShoppingHistory
 from shopping.history.shopping_item_purchase import ShoppingItemPurchase
@@ -9,12 +11,13 @@ class ShoppingHistoryDynamo(ShoppingHistory):
         self.table = table
 
     def _load_all_purchases(self) -> list:
-        return [ShoppingItemPurchase(name=x["id"],
+        return [ShoppingItemPurchase(name=x["name"],
                                      quantity=int(x["Quantity"]),
                                      purchase_datetime=string_to_datetime(x["Date"]))
                 for x in self.table.scan()["Items"]]
 
     def add_purchase(self, purchase: ShoppingItemPurchase):
-        self.table.put_item(Item={"id": purchase.name,
+        self.table.put_item(Item={"id": str(uuid.uuid4()),
+                                  "name": purchase.name,
                                   "Quantity": purchase.quantity,
                                   "Date": datetime_to_string(purchase.purchase_datetime)})
