88b2eeb7b8ee987a86e842215a339404a488678b
Leon Patmore <44168466+LeonPatmore@users.noreply.github.com>
2024-08-14 01:17:09 +0800

    Tests.

---
+++ tests/dynamo_db_mock.py
@@ -0,0 +1,27 @@
+# noinspection PyPep8Naming
+class DynamoDbMock:
+
+    def __init__(self):
+        self.items = []
+
+    def scan(self):
+        return {
+            "Items": self.items
+        }
+
+    def get_item(self, Key: dict):
+        return {
+            "Item": list(filter(lambda x: x["id"] == Key["id"], self.items))[0]
+        }
+
+    def put_item(self, Item: dict):
+        self.items.append(Item)
+
+    def update_item(self, Key: dict, UpdateExpression: str, ExpressionAttributeValues: dict, **_):
+        item = list(filter(lambda x: x["id"] == Key["id"], self.items))[0]
+        key = UpdateExpression.split("=")[0][4:]
+        item[key] = list(ExpressionAttributeValues.values())[0]
+
+    def delete_item(self, Key: dict):
+        item = list(filter(lambda x: x["id"] == Key["id"], self.items))[0]
+        self.items.remove(item)
